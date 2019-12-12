86b786b3ef2fc564bc376206618069e0fc121491
dz-s <shulhindvst@gmail.com>
2019-12-12 05:23:33 +0100

    rotate gif creation

--- main.py
+++ main.py
@@ -3,16 +3,63 @@
 Module Docstring
 """
 
-__author__ = "Your Name"
+__author__ = "dz_s"
 __version__ = "0.1.0"
 __license__ = "MIT"
 
+import cv2
+import sys
+import imageio
+import subprocess
+from PIL import Image
+
+def get_face_rect(image):
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    face_cascade = cv2.CascadeClassifier(
+        cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
+    faces = face_cascade.detectMultiScale(
+        gray,
+        scaleFactor=1.3,
+        minNeighbors=3,
+        minSize=(30, 30)
+    )
+    if len(faces):
+        return faces[0]
+    else:
+        raise Exception('No faces have been detected')
+
+def create_frames(cv2_image):
+    pil_image = Image.fromarray(cv2.cvtColor(cv2_image,cv2.COLOR_BGR2RGB))
+    frames = []
+    for i in range(7):
+        frames.append(pil_image.rotate(i*45))
+    return frames
+
+def create_gif(cv2_image):
+    path = 'res.gif'
+    frames = create_frames(cv2_image)
+    frames[0].save(path, format='GIF', append_images=frames[1:], save_all=True, duration=100, loop=0)
+    return path
+
+def get_optimized_gif(path):
+    subprocess.run(['gifsicle', '-O3', path, '-o', 'optimized.gif'], shell=True)
 
 def main():
     """ Main entry point of the app """
-    print("hello world")
-
+    try:
+        image_path = sys.argv[1]
+    except IndexError as e:
+        raise Exception('No path has been provided')
+    
+    print(image_path)
+ 
+    image = cv2.imread(image_path)
+    (x, y, w, h) = get_face_rect(image)
+    cropped = image [y:y+h, x:x+h]
+    path = create_gif(cropped)
+    get_optimized_gif(path)
 
 if __name__ == "__main__":
     """ This is executed when run from the command line """
     main()
+    
